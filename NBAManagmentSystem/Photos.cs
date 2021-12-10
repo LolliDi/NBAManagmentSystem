fce1b93816d48bbac184680c642e3fa994efbfa9
Lolli Di <290romchik@gmail.com>
2021-12-10 10:28:19 +0300

    set in photo

--- NBAManagmentSystem/Photos.cs
+++ NBAManagmentSystem/Photos.cs
@@ -7,16 +7,24 @@ namespace NBAManagmentSystem
     {
         int Id;
         public BitmapImage Photo = new BitmapImage();
+
+        public byte[] photo
+        {
+            set 
+            {
+                using (MemoryStream MS = new MemoryStream(value))
+                {
+                    Photo.BeginInit();
+                    Photo.StreamSource = MS;
+                    Photo.CacheOption = BitmapCacheOption.OnLoad;
+                    Photo.EndInit();
+                }
+            }
+        }
         public Photos(int id, byte[] b)
         {
             Id = id;
-            using(MemoryStream MS = new MemoryStream(b))
-            {
-                Photo.BeginInit();
-                Photo.StreamSource = MS;
-                Photo.CacheOption = BitmapCacheOption.OnLoad;
-                Photo.EndInit();
-            }
+            photo = b;
         }
 }
 }
